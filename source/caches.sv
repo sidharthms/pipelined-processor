7b8949115cd24f96a92fb15415ecb8b9adcf89a4
Akanksha Sharma <aks1834@gmail.com>
2014-10-22 17:43:59 -0400

    Cache design

--- source/caches.sv
+++ source/caches.sv
@@ -26,38 +26,37 @@ module caches (
   word_t instr;
 
   // icache
-  //icache  ICACHE(dcif, ccif);
+  icache  ICACHE(.CLK, .nRST, .dcif, .ccif);
   // dcache
-  //dcache  DCACHE(dcif, ccif);
+  //dcache  DCACHE(.CLK, .nRST, .dcif, .ccif);
 
   // single cycle instr saver (for memory ops)
-  always_ff @(posedge CLK)
-  begin
-    if (!nRST)
-    begin
-      instr <= '0;
-    end
-    else
-    if (!ccif.iwait[CPUID])
-    begin
-      instr <= ccif.iload[CPUID];
-    end
-  end
+//  always_ff @(posedge CLK)
+//  begin
+//    if (!nRST)
+//    begin
+//      instr <= '0;
+//    end
+//    else
+//    if (!ccif.iwait[CPUID])
+//    begin
+//      instr <= ccif.iload[CPUID];
+//    end
+//  end
   // dcache invalidate before halt
   assign dcif.flushed = dcif.halt;
 
   //single cycle
-  assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait[CPUID] : 0;
+//  assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait[CPUID] : 0;
   assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~ccif.dwait[CPUID] : 0;
-  assign dcif.imemload = (ccif.iwait[CPUID]) ? instr : ccif.iload[CPUID];
+//  assign dcif.imemload = (ccif.iwait[CPUID]) ? instr : ccif.iload[CPUID];
   assign dcif.dmemload = ccif.dload[CPUID];
 
 
-  assign ccif.iREN[CPUID] = dcif.imemREN;
+//  assign ccif.iREN[CPUID] = dcif.imemREN;
   assign ccif.dREN[CPUID] = dcif.dmemREN;
   assign ccif.dWEN[CPUID] = dcif.dmemWEN;
   assign ccif.dstore[CPUID] = dcif.dmemstore;
-  assign ccif.iaddr[CPUID] = dcif.imemaddr;
+//  assign ccif.iaddr[CPUID] = dcif.imemaddr;
   assign ccif.daddr[CPUID] = dcif.dmemaddr;
-
 endmodule
